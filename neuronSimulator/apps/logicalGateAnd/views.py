6d5c2a827ff4a542e1c3862a2d226aca2d0c5d7f
Ludwing Hernandez Caro <79872566+Ludwinghc@users.noreply.github.com>
2025-03-03 10:35:36 -0500

    feat: logic about neuron simulation

--- neuronSimulator/apps/logicalGateAnd/views.py
+++ neuronSimulator/apps/logicalGateAnd/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render, HttpResponse, redirect
-# Llamado a funciones
-from .utils import mi_funcion
+
+# Llamado a las clases
+from .services import Data, Perceptron, Trainer
 
 # Create your views here.
 def andGate(request):
-  message = ""
+  # Nuevo llamado a la instancia  de la data
+  dataset = Data()
+  # Nuevo llamado a la instancia del perpectron 
+  perceptron = Perceptron(input_size=2, alpha=0.1)
+  # Nuevo llamado a la instancia del trainer
+  trainer = Trainer(perceptron, dataset)
+  iterationList = []
+  inputIterationList = []
+  trainerOuputsValues = []
+  trainerErrorValues = []
+  iterationValue = 0
+  trainerInputValues = []
+  trainerPredictedValues = []
+  inputIteration = 0
   if request.method == "POST":	
-    message = mi_funcion()
-  return render(request, 'pages/andGate.html',{"mensaje":message})
+
+    
+    
+    # Llamado a los metodos del trainer para mostrar los resultados
+    trainerOuputsValues, trainerErrorValues, iterationValue = trainer.train()
+    trainerInputValues, trainerPredictedValues, inputIteration = trainer.evaluate()
+    iterationList = list(range(iterationValue))  # Lista de 0 a iterationValue-1
+    inputIterationList = list(range(inputIteration))
+    
+    
+  return render(
+    request,
+      'pages/andGate.html',
+      {
+        "dataInput": dataset.input_vector.tolist(),
+        "iteracion" : iterationList,
+        "inputIteration" : inputIterationList,
+        "Outputs" : trainerOuputsValues,
+        "Error" : trainerErrorValues,
+        "Inputs" : trainerInputValues,
+        "PredictedValues" : trainerPredictedValues,
+        })
